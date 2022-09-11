6fd2812d7c271e5ced2177fb494d7a6274762e46
serega <serega.135246@mail.ru>
2022-09-11 12:55:51 +0300

    copying old project

---
+++ simplekitchen/client-manager/src/main/java/com/simplekitchen/clientmanager/controller/api/MainScreenInfoController.java
@@ -0,0 +1,10 @@
+package com.simplekitchen.clientmanager.controller.api;
+
+
+import com.simplekitchen.dto.user.api.User;
+
+public interface MainScreenInfoController {
+
+
+    User getUser();
+}
---
+++ simplekitchen/dto/src/main/java/com/simplekitchen/dto/user/api/User.java
@@ -0,0 +1,8 @@
+package com.simplekitchen.dto.user.api;
+
+public interface User {
+    String getName();
+    String getSurname();
+    Long getAge();
+    String getSex();
+}
